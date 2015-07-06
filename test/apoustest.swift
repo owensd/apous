6bf68ffb4921a5da9583ac1894e44d53c5dacabf
David Owens II <david@owensd.io>
2015-07-06 10:34:48 -0700

    Added initial test target.

---
+++ test/apoustest.swift
@@ -0,0 +1,28 @@
+//
+//  apoustest.swift
+//  apoustest
+//
+//  Created by David Owens on 7/6/15.
+//  Copyright © 2015 owensd.io. All rights reserved.
+//
+
+import XCTest
+
+class apoustest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
